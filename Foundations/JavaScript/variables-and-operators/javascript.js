a24ee9e580d2b8f553be5e9675b6fab3aefcc22d
Zach West <zwest2563@gmail.com>
2025-05-29 11:54:04 -0500

    Practice(Var): How to create JS file & assign var

---
+++ Foundations/JavaScript/variables-and-operators/javascript.js
@@ -0,0 +1,26 @@
+
+let surname = "Snow";
+let firstName = "Jon";
+let age = 14;
+
+console.log(firstName);
+console.log(surname);
+console.log(age);
+age = 54;
+console.log(age);
+
+const pi = 3.14159;
+// pi = 10; ERROR... cant reassign a constant
+console.log(pi);
+
+let a = 10;
+console.log(a);
+a =  50;
+let b = 7 * a;
+console.log(b);
+
+
+let max = 57;
+let actual = max - 13;
+let percentage = actual / max;
+console.log(percentage);
