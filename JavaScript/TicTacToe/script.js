c1b0cb6c7f3ab85ee4cfee95dac770248cda4c55
Zach West <zwest2563@gmail.com>
2025-07-02 11:42:38 -0500

    Feat: Working Text-Based Tic Tac Toe Game

--- JavaScript/TicTacToe/script.js
+++ JavaScript/TicTacToe/script.js
@@ -2,11 +2,17 @@
 function CreateGame(){
     // Private Variables
     const GameBoard = [ ["-","-","-"], ["-","-","-"], ["-","-","-"]];
+    let gameTurn = 0;
     return{
         // Public Variables/Methods
-        makeMove(row,col){
+        CreatePlayer: function (playerSymbol){
+            return{
+                playerSymbol
+            }
+        },
+        makeMove(player,row,col){
             if (GameBoard[row][col] == "-"){
-                GameBoard[row][col] = playerSymbol;
+                GameBoard[row][col] = player.playerSymbol;
             }
         },
         checkWin(board, symbol){
@@ -31,15 +37,67 @@ function CreateGame(){
             else{
                 return false;
             }
+        },
+        playGame(){
+            alert("Welcome to Zic-Zac-Zoe!");
+            let playerOne = this.CreatePlayer("X");
+            let playerTwo = this.CreatePlayer("O");
+            let userInput = "";
+            let isGameOver = false;
+
+            let turnTracker = 1;
+            while (1){
+                if (this.checkWin(GameBoard,playerOne.playerSymbol)){
+                    return 1;
+                }
+                else if (this.checkWin(GameBoard,playerTwo.playerSymbol)){
+                    return 2;
+                }
+                else if (gameTurn == 9){
+                    return -1;
+                }
+                userInput = prompt(`Please Enter Your Move!\n
+                                    ${GameBoard[0][0]}|${GameBoard[0][1]}|${GameBoard[0][2]}\n
+                                    ${GameBoard[1][0]}|${GameBoard[1][1]}|${GameBoard[1][2]}\n
+                                    ${GameBoard[2][0]}|${GameBoard[2][1]}|${GameBoard[2][2]}\n
+                                `);
+                const userPair = userInput.split(" "); // ["0", "2"]
+                const coordinates = userPair.map(Number); // [0, 2]
+
+                while (coordinates[0] > 2 || coordinates[0] < 0 || coordinates[1] > 2 || coordinates[1] < 0 ){
+                    alert("Your input is outside the proper parameters")
+                    userInput = prompt(`Please Enter Your Move!\n${GameBoard}\n`);
+                    userPair = userInput.split(" "); // ["0", "2"]
+                    coordinates = userPair.map(Number); // [0, 2]
+                    if ( GameBoard[ coordinates[0] ][ GameBoard[ coordinates[1] ] ] != '-' ){
+                        //manually set the coordinates out of bound to reprompt entry
+                        coordinates[0] = 5;
+                        coordinates[1] = 5;
+                    }
+                }
+                if (turnTracker == 1){
+                    this.makeMove(playerOne,coordinates[0],coordinates[1]);
+                }
+                else{
+                    this.makeMove(playerTwo,coordinates[0],coordinates[1]);
+                }
+                turnTracker *= -1;
+                gameTurn +=1;
+                
+        }
         }
     }
+
 }
-function CreatePlayer(playerSymbol){
-    return{
-        playerSymbol,
-        
-    }
-}
+
+
+let game = CreateGame();
+
+let outcome = game.playGame();
+
+if (outcome == 1) console.log ("Player One Wins!");
+if (outcome == 2) console.log ("Player Two Wins!");
+if (outcome == -1) console.log ("Tie!");
 
 
 
